133b4e0bc1a0769d01b3887723b42cf04f806c1a
Nicolas Lapointe <nixcocpp@gmail.com>
2018-10-24 20:42:37 -0400

    ajout de propriétés au model User ainsi que documentation des models

--- client/src/app/user-management/models/authPayload.ts
+++ client/src/app/user-management/models/authPayload.ts
@@ -1,3 +1,9 @@
+/**
+ * Data payload used when logging in and registering
+ *
+ * @export
+ * @interface AuthPayload
+ */
 export default interface AuthPayload {
   email: string;
   passwd: string;
--- client/src/app/user-management/models/user.ts
+++ client/src/app/user-management/models/user.ts
@@ -1,8 +1,20 @@
+/**
+ * Represents the Token html response from the login API route
+ *
+ * @export
+ * @interface TokenResponse
+ */
 export interface TokenResponse {
   token: string;
 }
 
-export class UserToken {
+/**
+ * User model, representation of the info contained in the JWT token format returned by the API
+ *
+ * @export
+ * @class User
+ */
+export class User {
   private _username: string;
   public get username(): string {
     return this._username;
@@ -25,5 +37,13 @@ export class UserToken {
   }
   public set isAdmin(v: boolean) {
     this._isAdmin = v;
+  }
+
+  private _exp: number;
+  public get exp(): number {
+    return this._exp;
+  }
+  public set exp(v: number) {
+    this._exp = v;
   }
 }
